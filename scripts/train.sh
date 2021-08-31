919c03c61e6b588d95226b6a1e6d744161f0ed75
Bodomit <gbrown29@qub.ac.uk>
2021-08-31 22:07:18 +0100

    Multiple LR training script.

--- scripts/train.sh
+++ scripts/train.sh
@@ -9,8 +9,35 @@
 #SBATCH --time=3-0
 #SBATCH --signal=SIGUSR1@90
 
-module add nvidia-cuda
-conda activate houshou3
+# Invoke with sbatch --array=0-3 ./scripts/train.sh $RESULTS_ROOT_DIR $PRETRAINED_PATH
 
-srun python -m train /mnt/scratch2/users/40057686/results/jlu-train -b 256
+module add nvidia-cuda
+
+RESULTS_ROOT_DIR=$1
+echo "RESULTS_ROOT_DIR: $RESULTS_ROOT_DIR"
+
+PRETRAINED_PATH=$2
+echo "PRETRAINED_PATH: $PRETRAINED_PATH"
+
+LR_ID=${SLURM_ARRAY_TASK_ID:-0}
+
+echo "ALPHA_ID: $ALPHA_ID"
+
+LRS=(1e-5 1e-4 1e-3 1e-2)
+LR=${LRS[$LR_ID]}
+
+RESULTSDIR=$RESULTS_ROOT_DIR/jlu-train/$LR
+
+echo "LR: $LR"
+echo "RESULTSDIR: $RESULTSDIR"
+
+echo "CPU Stats"
+python -c "import os; print('CPUS: ', len(os.sched_getaffinity(0)))"
+echo ""
+
+echo "GPU Stats:"
+nvidia-smi
+echo ""
+
+srun python -m train $RESULTSDIR -b 256 --pretrained $PRETRAINED_PATH -lr $LR
 
