eaf357ac5e6bda752cf640e5dde0e5a024fa531f
bodomit <gbrown29@qub.ac.uk>
2021-09-03 15:52:07 +0100

    Added script for simple trainer.

--- scripts/train_primary.sh
+++ scripts/train_primary.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=JLU-Train
+#SBATCH --job-name=JLU-TrainPrimary
 #SBATCH --partition=k2-gpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64GB
 #SBATCH --gres=gpu:1
-#SBATCH --output=/mnt/scratch2/users/40057686/logs/jlu-train/%A-%a.log
+#SBATCH --output=/mnt/scratch2/users/40057686/logs/jlu-train-primary/%A-%a.log
 #SBATCH --time=3-0
 #SBATCH --signal=SIGUSR1@90
 
-# Invoke with sbatch --array=0-3 ./scripts/train.sh $RESULTS_ROOT_DIR $PRETRAINED_PATH
+# Invoke with sbatch --array=0-4 ./scripts/train_primary.sh $RESULTS_ROOT_DIR $PRETRAINED_PATH
 
 module add nvidia-cuda
 
@@ -23,10 +23,10 @@ LR_ID=${SLURM_ARRAY_TASK_ID:-0}
 
 echo "ALPHA_ID: $ALPHA_ID"
 
-LRS=(1e-5 1e-4 1e-3 1e-2)
+LRS=(1e-4 1e-3 1e-5 1e-6 1e-2)
 LR=${LRS[$LR_ID]}
 
-RESULTSDIR=$RESULTS_ROOT_DIR/jlu-train/$LR
+RESULTSDIR=$RESULTS_ROOT_DIR/jlu-train-primary/$LR
 
 echo "LR: $LR"
 echo "RESULTSDIR: $RESULTSDIR"
@@ -39,5 +39,5 @@ echo "GPU Stats:"
 nvidia-smi
 echo ""
 
-srun python -m train $RESULTSDIR -b 128 --pretrained $PRETRAINED_PATH -lr $LR
+srun python -m train $RESULTSDIR --primary-only -b 128 --pretrained $PRETRAINED_PATH -lr $LR
 
